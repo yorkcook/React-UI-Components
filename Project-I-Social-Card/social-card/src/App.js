e3f9f0542d51ac857f51e890582f06dd36a86082
York Cook <yorkcook@Johnny-Castle.local>
2019-05-28 18:30:11 -0400

    WIP: all content added

--- Project-I-Social-Card/social-card/src/App.js
+++ Project-I-Social-Card/social-card/src/App.js
@@ -5,11 +5,20 @@ import HeaderContent from './components/HeaderComponents/HeaderContent';
 
 import ImageThumbnail from './components/HeaderComponents/ImageThumbnail';
 
+import CardBanner from './components/CardComponents/CardBanner'
+
+import CardContent from './components/CardComponents/CardContent'
+
 const App = () => {
   return (
     <div>
         <ImageThumbnail />
+        
         <HeaderContent />
+        
+        <CardBanner />
+
+        <CardContent />
         
     </div>
   );
