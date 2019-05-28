6536aa8480f6580fde95060a2872431fac4f71ce
York Cook <yorkcook@Johnny-Castle.local>
2019-05-28 18:20:02 -0400

    WIP: logo added

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/ImageThumbnail.js
@@ -5,6 +5,6 @@ import App from '../../App';
 const ImageThumbnail = () => {
     return  <img src= "https://tk-assets.lambdaschool.com/1c1b7262-cf23-4a9f-90b6-da0d3c74a5c6_lambdacrest.png" />
  
-}
+};
 
-export default App;
+export default ImageThumbnail;
